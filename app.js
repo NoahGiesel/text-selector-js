ad9634ade1818156bc792f2e39762de998737c19
noah <noah@noahs-Mac.local>
2020-04-15 11:26:06 -0700

    css and js moved

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
  
+    const textarea = document.getElementById("Textarea") ;
+
+
+    function getSelectedText() { 
+         alert(textarea) 
+        if (window.getSelection) {
+            return window.getSelection().toString();
+        } else if (document.selection) {
+            return document.selection.createRange().text;
+        }
+        return '';
+    }
+    var b = document.getElementsByTagName('body')[0],
+        o = document.getElementById('output');
+    b.onmouseup = function(e){
+        var selText = getSelectedText(),
+            targetElem = e.target.tagName.toLowerCase();
+        if (selText && targetElem == 'p') {
+            o.textContent = 'You selected the text: "' + selText + '" from a ' + targetElem + ' element.';
+        }
+    };
